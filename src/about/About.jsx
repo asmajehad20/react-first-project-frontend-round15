b9e771c67dbfca13f60fbe4181a89f204620635a
asmajehad20 <asmajehad919@gmail.com>
2026-01-17 15:17:57 +0200

    about

--- src/about/About.jsx
+++ src/about/About.jsx
@@ -4,6 +4,24 @@ import style from './About.module.css'
 export default function About() {
     console.log(style);
   return (
-    <div>About</div>
+    <section className={style.about}>
+      <h2 className={style.title}>About</h2>
+
+      <div className={style.content}>
+        <p className={style.paragraph}>
+          Freelancer is a free Bootstrap theme created by Start Bootstrap. 
+          The download includes the complete source files including HTML, CSS, and JavaScript 
+          as well as optional SASS stylesheets for easy customization.
+        </p>
+
+        <p className={style.paragraph}>
+          You can create your own custom avatar for the masthead, change the icon in the dividers, 
+          and add your email address to the contact form to make it fully functional!
+        </p>
+      </div>
+      <a href="#" className={style.downloadButton}>
+        <i className="bi bi-download"></i> Free Download
+      </a>
+    </section>
   )
 }
